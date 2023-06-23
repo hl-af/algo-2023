e670db6a414719c9569cfcdb4a620b04e927d013
hl-af <5058502916@qq.com>
2023-06-23 11:04:05 +0800

    二叉树从底层遍历

--- src/main/java/tree/LevleScanTree.java
+++ src/main/java/tree/LevleScanTree.java
@@ -3,9 +3,7 @@ package tree;
 import com.sun.source.tree.ArrayAccessTree;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class LevleScanTree {
 
@@ -38,12 +36,59 @@ public class LevleScanTree {
 
     }
 
+
+
     @Test
     public void testSimpleLevelOrder() {
         TreeNode root = TreeUtils.getTreeDemo();
         List<Integer> res = simpleLevelOrder(root);
         for (int i = 0; i < res.size(); i++) {
             System.out.print(res.get(i) + ",");
+        }
+    }
+
+
+    /**
+     * LeetCode 107.给定⼀个⼆叉树，返回其节点值⾃底向上的层序遍历。
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>(); // 使用queue，不是LinkedList
+        List<List<Integer>> res = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) { // 使用 isEmpty 判断，不是size
+
+            List<Integer> level = new LinkedList<>();
+            int length = queue.size();
+            for (int i = 0; i < length; i++) {
+                TreeNode temp = queue.poll();
+                if (temp.left != null) {
+                    queue.add(temp.left);
+                }
+                if (temp.right != null) {
+                    queue.add(temp.right);
+                }
+                level.add(temp.val);
+            }
+            res.add(0, level);
+
+        }
+        return res;
+
+    }
+
+    @Test
+    public void testLevelOrderBottom() {
+
+        TreeNode root = TreeUtils.getTreeDemo();
+        List<List<Integer>> res = levelOrderBottom(root);
+        for (int i = 0; i < res.size(); i++) {
+            List<Integer> level = res.get(i);
+            for (int j = 0; j < level.size(); j++) {
+                System.out.print(level.get(j) + ",");
+            }
+            System.out.println();
         }
     }
     
